9d469857a12a75b41fd089a7bcac158b2456247a
sounos <sounos@7d4f2944-f531-0410-9e72-0badb3dd7d62>
2008-02-25 10:24:36 +0000

    added basedef.h

---
+++ devel/libsbase/src/basedef.h
@@ -0,0 +1,13 @@
+#ifndef _BASEDEF_H
+#define _BASEDEF_H
+#define CMD_TRUNCATE    0
+#define CMD_PUT         1
+#define CMD_DEL         2
+#define CMD_MD5SUM      3
+#ifndef PATH_MAX_SIZE
+#define PATH_MAX_SIZE   256
+#endif
+#define MD5SUM_SIZE    32
+#define TBLOCK_SIZE    65536
+#endif
+
