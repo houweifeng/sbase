fd7b2dcc7ca214d6f3d915d02d36d165fc91080a
sounos <sounos@7d4f2944-f531-0410-9e72-0badb3dd7d62>
2008-04-22 01:21:59 +0000

    Fixed messagelist result crash bug

--- trunk/devel/libsbase/src/utils/message.h
+++ trunk/devel/libsbase/src/utils/message.h
@@ -13,12 +13,12 @@ extern "C" {
 #ifndef _TYPEDEF_MESSAGE
 #define _TYPEDEF_MESSAGE
 /* MESSAGE ACTION ID DEFINE */
-#define MESSAGE_QUIT            0x20
 #define MESSAGE_NEW_SESSION     0x01
 #define MESSAGE_INPUT           0x02
 #define MESSAGE_OUTPUT          0x04
 #define MESSAGE_PACKET          0x08
 #define MESSAGE_DATA            0x10
+#define MESSAGE_QUIT            0x20
 #define MESSAGE_TRANSACTION     0x40
 #define MESSAGE_TASK            0x80
 #define MESSAGE_HEARTBEAT       0x100
@@ -27,18 +27,20 @@ extern "C" {
 #define MESSAGE_ALL		(MESSAGE_QUIT | MESSAGE_NEW_SESSION | MESSAGE_INPUT | MESSAGE_OUTPUT \
 				| MESSAGE_PACKET | MESSAGE_DATA | MESSAGE_TRANSACTION| MESSAGE_TASK \
                 | MESSAGE_HEARTBEAT | MESSAGE_STATE | MESSAGE_DSTATE)
+#define MESSAGE_NUM 11
 static char *messagelist[] = 
 {
-	"",
 	"MESSAGE_NEW_SESSION",
 	"MESSAGE_INPUT",
-	"",
 	"MESSAGE_OUTPUT",
-	"","","",
 	"MESSAGE_PACKET",
-	"","","","","","","",
-	"MESSAGE_DATA","","","","","","","","","","","","","","","",
-	"MESSAGE_QUIT"
+	"MESSAGE_DATA",
+	"MESSAGE_QUIT",
+	"MESSAGE_TRANSACTION",
+	"MESSAGE_TASK",
+	"MESSAGE_HEARTBEAT",
+	"MESSAGE_STATE",
+	"MESSAGE_DSTATE"
 };
 typedef struct _MESSAGE
 {
@@ -53,6 +55,7 @@ typedef struct _MESSAGE
 #define MESSAGE_INIT() ((MESSAGE *)calloc(1, sizeof(MESSAGE)))
 #define MESSAGE_CLEAN(ptr) {if(ptr){free(ptr);ptr = NULL;}}
 #define MESSAGE_SIZE    sizeof(MESSAGE)
+#define MESSAGE_DESC(id) ((id >= 0 && id < MESSAGE_NUM)?messagelist[] : "")
 #endif
 #ifdef __cplusplus
  }
