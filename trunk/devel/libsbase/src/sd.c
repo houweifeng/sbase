e25a00412d397c9e639766f747deac420a79ebf0
sounos <sounos@7d4f2944-f531-0410-9e72-0badb3dd7d62>
2008-05-05 13:05:27 +0000

    Updated service.c

--- trunk/devel/libsbase/src/sd.c
+++ trunk/devel/libsbase/src/sd.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <sbase.h>
 #include <errno.h>
+#include "sbase.h"
 
 int cb_packet_reader(CONN *conn, BUFFER *buffer)
 {
@@ -10,7 +10,7 @@ int cb_packet_reader(CONN *conn, BUFFER *buffer)
 void cb_packet_handler(CONN *conn, BUFFER *packet)
 {
             if(conn && conn->push_chunk)
-                                conn->push_chunk((CONN *)conn, ((BUFFER *)packet)->data, packet->size);
+                conn->push_chunk((CONN *)conn, ((BUFFER *)packet)->data, packet->size);
 }
 
 void cb_data_handler(CONN *conn, BUFFER *packet, CHUNK *chunk, BUFFER *cache)
@@ -25,21 +25,26 @@ int main()
 {
     SBASE *sbase = sbase_init();
     sbase->working_mode = 1;
+    sbase->max_procthreads = 32;
     sbase->sleep_usec = 10000;
-    sbase->set_log(sbase, "/tmp/abcd.log");
+    sbase->set_log(sbase, "/tmp/sb.log");
+    sbase->set_evlog(sbase, "/tmp/sb.evlog");
     SERVICE *service = service_init();
+    service->service_type = S_SERVICE;
     service->family = AF_INET;
     service->socket_type = SOCK_STREAM;
-    service->service_type = S_SERVICE;
-    service->ip=NULL;
-    service->logfile = "/tmp/abc.log";
-    service->port=62800;
-    service->working_mode = 1;
+    service->name = "sd";
+    service->ip = NULL;
+    service->port = 1418;
     service->max_procthreads = 2;
+    service->logfile = "/tmp/sd.log";
+    service->evlogfile = "/tmp/sd.evlog";
     service->sleep_usec = 10000;
+    service->heartbeat_interval = 100000;
     service->max_connections = 1024;
     service->packet_type = PACKET_DELIMITER;
     service->packet_delimiter = "\r\n";
+    service->packet_delimiter_length = 2;
     service->buffer_size = 1024 * 1024;
     service->cb_packet_reader = &cb_packet_reader;
     service->cb_packet_handler = &cb_packet_handler;
