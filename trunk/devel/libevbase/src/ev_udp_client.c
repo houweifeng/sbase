b10980d79912200f444e0eeaa00cb63a621a22ad
sounos <sounos@7d4f2944-f531-0410-9e72-0badb3dd7d62>
2008-05-05 13:43:21 +0000

    added new clean libevbase code

---
+++ trunk/devel/libevbase/src/ev_udp_client.c
@@ -0,0 +1,174 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include "evbase.h"
+#include "log.h"
+#include "logger.h"
+#define CONN_MAX 10240
+#define EV_BUF_SIZE 128
+EVBASE *evbase = NULL;
+char buffer[CONN_MAX][EV_BUF_SIZE];
+EVENT *events[CONN_MAX];
+
+int setrlimiter(char *name, int rlimit, int nset)
+{
+    int ret = -1;
+    struct rlimit rlim;
+    if(name)
+    {
+        if(getrlimit(rlimit, &rlim) == -1)
+            return -1;
+        if(rlim.rlim_cur > nset && rlim.rlim_max > nset)
+            return 0;
+        rlim.rlim_cur = nset;
+        rlim.rlim_max = nset;
+        if((ret = setrlimit(rlimit, &rlim)) == 0)
+        {
+            fprintf(stdout, "setrlimit %s cur[%ld] max[%ld]\n",
+                    name, rlim.rlim_cur, rlim.rlim_max);
+            return 0;
+        }
+        else
+        {
+            fprintf(stderr, "setrlimit %s cur[%ld] max[%ld] failed, %s\n",
+                    name, rlim.rlim_cur, rlim.rlim_max, strerror(errno));
+        }
+    }
+    return ret;
+}
+
+void ev_handler(int fd, short ev_flags, void *arg)
+{
+	int n = 0;
+	struct sockaddr_in rsa;
+	socklen_t rsa_len ;
+	if(ev_flags & E_READ)
+	{
+		if( ( n = recvfrom(fd, buffer[fd], EV_BUF_SIZE, 0, (struct sockaddr *)&rsa, &rsa_len)) > 0 )
+		{
+			SHOW_LOG("Read %d bytes from %d", n, fd);
+			buffer[fd][n] = 0;
+			SHOW_LOG("Updating event[%x] on %d ", events[fd], fd);
+			if(events[fd])
+			{
+				events[fd]->add(events[fd], E_WRITE);	
+			}
+		}		
+		else
+		{
+			if(n < 0 )
+				FATAL_LOG("Reading from %d failed, %s", fd, strerror(errno));
+			goto err;
+		}
+	}
+	if(ev_flags & E_WRITE)
+	{
+		if(  (n = write(fd, buffer[fd], strlen(buffer[fd])) ) > 0 )
+		{
+			SHOW_LOG("Wrote %d bytes via %d", n, fd);
+		}
+		else
+		{
+			if(n < 0)
+				FATAL_LOG("Wrote data via %d failed, %s", fd, strerror(errno));	
+			goto err;
+		}
+		if(events[fd]) events[fd]->del(events[fd], E_WRITE);
+	}
+	return ;
+	err:
+	{
+		if(events[fd])
+		{
+			events[fd]->destroy(events[fd]);
+			events[fd] = NULL;
+			shutdown(fd, SHUT_RDWR);
+			close(fd);
+			SHOW_LOG("Connection %d closed", fd);
+		}
+
+	}
+}
+
+int main(int argc, char **argv)
+{
+	char *ip = NULL;
+	int port = 0;
+	int fd = 0;
+	struct sockaddr_in sa, lsa;	
+	socklen_t sa_len, lsa_len = -1;
+	int opt = 1;
+	int i = 0;
+	int conn_num = 0;
+	EVENT  *event = NULL;
+	if(argc < 4)
+	{
+		fprintf(stderr, "Usage:%s ip port connection_number\n", argv[0]);	
+		_exit(-1);
+	}	
+	ip = argv[1];
+	port = atoi(argv[2]);
+	conn_num = atoi(argv[3]);
+	/* Set resource limit */
+	setrlimiter("RLIMIT_NOFILE", RLIMIT_NOFILE, CONN_MAX);	
+	/* Initialize global vars */
+	memset(events, 0, sizeof(EVENT *) * CONN_MAX);
+	/* Initialize inet */ 
+	memset(&sa, 0, sizeof(struct sockaddr_in));	
+	sa.sin_family = AF_INET;
+	sa.sin_addr.s_addr = inet_addr(ip);
+	sa.sin_port = htons(port);
+	sa_len = sizeof(struct sockaddr);
+        /* set evbase */
+        if((evbase = evbase_init()))
+	{
+		LOGGER_INIT(evbase->logger, "/tmp/ev_udp_client.log");
+		while((fd = socket(AF_INET, SOCK_DGRAM, 0)) > 0 && fd < conn_num)
+		{
+			/* Set REUSE */
+			//setsockopt(fd, SOL_SOCKET, SO_REUSEADDR,
+                        //	(char *)&opt, (socklen_t) sizeof(opt) );
+			/* Bind */
+			//if(lsa_len != -1)
+			//	bind(fd, (struct sockaddr *)&lsa, lsa_len);
+			/* Connect */
+			if(connect(fd, (struct sockaddr *)&sa, sa_len) == 0 )
+			{
+				/* set FD NON-BLOCK */
+                                fcntl(fd, F_SETFL, O_NONBLOCK);
+				if((events[fd] = ev_init()))
+				{
+					events[fd]->set(events[fd], fd, E_READ|E_WRITE|E_PERSIST, 
+						(void *)events[fd], &ev_handler);
+					evbase->add(evbase, events[fd]);
+					sprintf(buffer[fd], "%d:client message", fd);
+				}
+				lsa_len = sizeof(struct sockaddr_in);
+				memset(&lsa, 0, lsa_len);
+				getsockname(fd, (struct sockaddr *)&lsa, &lsa_len);
+				SHOW_LOG("Connected to %s:%d via %d port:%d", ip, port, fd, ntohs(lsa.sin_port));
+			}
+			else
+			{
+				FATAL_LOG("Connect to %s:%d failed, %s", ip, port, strerror(errno));
+				break;
+				//_exit(-1);
+			}
+		}
+		while(1)
+		{
+			evbase->loop(evbase, 0, NULL);
+			usleep(10);
+		}
+	}
+}
---
+++ trunk/devel/libevbase/src/evbase.c
@@ -0,0 +1,315 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include "logger.h"
+#include "evbase.h"
+#ifdef HAVE_EVPORT
+#include "evport.h"
+#endif
+#ifdef HAVE_EVSELECT
+#include "evselect.h"
+#endif
+#ifdef HAVE_EVPOLL
+#include "evpoll.h"
+#endif
+#ifdef HAVE_EVRTSIG
+#include "evrtsig.h"
+#endif
+#ifdef HAVE_EVEPOLL
+#include "evepoll.h"
+#endif
+#ifdef HAVE_EVKQUEUE
+#include "evkqueue.h"
+#endif
+#ifdef HAVE_EVDEVPOLL
+#include "evdevpoll.h"
+#endif
+#ifdef WIN32
+#include "evwin32.h"
+#endif
+
+typedef struct _EVOPS
+{
+    char    *name ;
+    int     (*init)(struct _EVBASE *);
+    int     (*add)(struct _EVBASE *, struct _EVENT*);
+    int     (*update)(struct _EVBASE *, struct _EVENT*);
+    int     (*del)(struct _EVBASE *, struct _EVENT*);
+    void    (*loop)(struct _EVBASE *, short , struct timeval *tv);
+    void    (*reset)(struct _EVBASE *);
+    void    (*clean)(struct _EVBASE **);
+}EVOPS;
+static EVOPS evops[EOP_LIMIT] = {0};
+static int evops_default =      -1;
+/* Set event */
+void event_set(EVENT *event, int fd, short flags, void *arg, void *handler);
+/* Add event */
+void event_add(EVENT *event, short flags);
+/* Delete event */
+void event_del(EVENT *event, short flags);
+/* Active event */
+void event_active(EVENT *event, short ev_flags);
+/* Destroy event */
+void event_destroy(EVENT *event);
+/* Clean event */
+void event_clean(EVENT **event);
+
+/* set logfile */
+void evbase_set_logfile(EVBASE *evbase, char *file)
+{
+    if(evbase)
+    {
+        LOGGER_INIT(evbase->logger, file);
+        DEBUG_LOGGER(evbase->logger, "Initailize evlog[%s]", file);
+    }
+    return ;
+}
+/* set event operating */
+int evbase_set_evops(EVBASE *evbase, int evopid)
+{
+    if(evbase)
+    {
+        if(evopid >= 0 && evopid < EOP_LIMIT && evops[evopid].name != NULL)
+        {
+            if(evbase->reset)evbase->reset(evbase);
+            if(evops[evopid].init) evbase->init = evops[evopid].init;
+            if(evops[evopid].add) evbase->add = evops[evopid].add;
+            if(evops[evopid].update) evbase->update = evops[evopid].update;
+            if(evops[evopid].del) evbase->del = evops[evopid].del;
+            if(evops[evopid].loop) evbase->loop = evops[evopid].loop;
+            if(evops[evopid].reset) evbase->reset = evops[evopid].reset;
+            if(evops[evopid].clean) evbase->clean = evops[evopid].clean;
+            if(evbase->init(evbase) == -1)
+                evbase->set_evops(evbase, evops_default);
+            DEBUG_LOGGER(evbase->logger, "Set event to [%s]", evops[evopid].name);
+            return 0;
+        }
+    }
+    return -1;
+}
+
+/* Initialize evbase */
+EVBASE *evbase_init()
+{
+	EVBASE *evbase = (EVBASE *)calloc(1, sizeof(EVBASE));	
+	char *s = NULL;
+	if(evbase)
+    {
+#ifdef HAVE_EVPORT
+        evops_default = EOP_PORT;
+        evops[EOP_PORT].name = "PORT";
+        evops[EOP_PORT].init      = evport_init;
+        evops[EOP_PORT].add       = evport_add;
+        evops[EOP_PORT].update    = evport_update;
+        evops[EOP_PORT].del       = evport_del;
+        evops[EOP_PORT].loop      = evport_loop;
+        evops[EOP_PORT].reset     = evport_reset;
+        evops[EOP_PORT].clean     = evport_clean;
+#endif
+#ifdef HAVE_EVSELECT
+        evops_default = EOP_SELECT;
+        evops[EOP_SELECT].name    = "SELECT";
+        evops[EOP_SELECT].init    = evselect_init;
+        evops[EOP_SELECT].add     = evselect_add;
+        evops[EOP_SELECT].update  = evselect_update;
+        evops[EOP_SELECT].del     = evselect_del;
+        evops[EOP_SELECT].loop    = evselect_loop;
+        evops[EOP_SELECT].reset   = evselect_reset;
+        evops[EOP_SELECT].clean   = evselect_clean;
+#endif
+#ifdef HAVE_EVPOLL
+        evops_default = EOP_POLL;
+        evops[EOP_POLL].name      = "POLL";
+        evops[EOP_POLL].init      = evpoll_init;
+        evops[EOP_POLL].add       = evpoll_add;
+        evops[EOP_POLL].update    = evpoll_update;
+        evops[EOP_POLL].del       = evpoll_del;
+        evops[EOP_POLL].loop      = evpoll_loop;
+        evops[EOP_POLL].reset     = evpoll_reset;
+        evops[EOP_POLL].clean     = evpoll_clean;
+#endif
+#ifdef HAVE_EVRTSIG
+        evops_default = EOP_RTSIG;
+        evops[EOP_RTSIG].name     = "RTSIG" ;
+        evops[EOP_RTSIG].init     = evrtsig_init;
+        evops[EOP_RTSIG].add      = evrtsig_add;
+        evops[EOP_RTSIG].update   = evrtsig_update;
+        evops[EOP_RTSIG].del      = evrtsig_del;
+        evops[EOP_RTSIG].loop     = evrtsig_loop;
+        evops[EOP_RTSIG].reset    = evrtsig_reset;
+        evops[EOP_RTSIG].clean    = evrtsig_clean;
+#endif
+#ifdef HAVE_EVEPOLL
+        evops_default = EOP_EPOLL;
+        evops[EOP_EPOLL].name     = "EPOLL";
+        evops[EOP_EPOLL].init     = evepoll_init;
+        evops[EOP_EPOLL].add      = evepoll_add;
+        evops[EOP_EPOLL].update   = evepoll_update;
+        evops[EOP_EPOLL].del      = evepoll_del;
+        evops[EOP_EPOLL].loop     = evepoll_loop;
+        evops[EOP_EPOLL].reset    = evepoll_reset;
+        evops[EOP_EPOLL].clean    = evepoll_clean;
+#endif
+#ifdef HAVE_EVKQUEUE
+        evops_default = EOP_KQUEUE;
+        evops[EOP_KQUEUE].name    = "KQUEUE";
+        evops[EOP_KQUEUE].init    = evkqueue_init;
+        evops[EOP_KQUEUE].add     = evkqueue_add;
+        evops[EOP_KQUEUE].update  = evkqueue_update;
+        evops[EOP_KQUEUE].del     = evkqueue_del;
+        evops[EOP_KQUEUE].loop    = evkqueue_loop;
+        evops[EOP_KQUEUE].reset   = evkqueue_reset;
+        evops[EOP_KQUEUE].clean   = evkqueue_clean;
+#endif
+#ifdef HAVE_EVDEVPOLL
+        evops_default = EOP_DEVPOLL;
+        evops[EOP_DEVPOLL].name   = "/dev/poll";
+        evops[EOP_DEVPOLL].init   = evdevpoll_init;
+        evops[EOP_DEVPOLL].add    = evdevpoll_add;
+        evops[EOP_DEVPOLL].update = evdevpoll_update;
+        evops[EOP_DEVPOLL].del    = evdevpoll_del;
+        evops[EOP_DEVPOLL].loop   = evdevpoll_loop;
+        evops[EOP_DEVPOLL].reset  = evdevpoll_reset;
+        evops[EOP_DEVPOLL].clean  = evdevpoll_clean;
+#endif
+#ifdef WIN32
+        evops_default = EOP_WIN32;
+        evops[EOP_WIN32].name     = "WIN32";
+        evops[EOP_WIN32].init     = evwin32_init;
+        evops[EOP_WIN32].add      = evwin32_add;
+        evops[EOP_WIN32].update   = evwin32_update;
+        evops[EOP_WIN32].del      = evwin32_del;
+        evops[EOP_WIN32].loop     = evwin32_loop;
+        evops[EOP_WIN32].reset    = evwin32_reset;
+        evops[EOP_WIN32].clean    = evwin32_clean;
+#endif
+
+
+        evbase->set_logfile = evbase_set_logfile;
+        evbase->set_evops   = evbase_set_evops;
+        //evbase->clean 	=  evbase_clean;
+        if(evops_default == -1 || evbase->set_evops(evbase, evops_default) == -1)
+        {
+            free(evbase); 
+            fprintf(stderr, "Initialize evbase to default[%d] failed, %s\n", 
+                    evops_default, strerror(errno));
+            evbase = NULL;
+        }
+    }
+	return evbase;
+}
+
+/* Initialize event */
+EVENT *ev_init()
+{
+	EVENT *event = (EVENT *)calloc(1, sizeof(EVENT));
+	if(event)
+	{
+		event->set 	= event_set;
+		event->add	= event_add;
+		event->del	= event_del;
+		event->active	= event_active;
+		event->destroy	= event_destroy;
+		event->clean	= event_clean;
+	}
+	return event;
+}
+
+/* Set event */
+void event_set(EVENT *event, int fd, short flags, void *arg, void *handler)
+{
+	if(event)
+	{
+		if(fd > 0 && handler)
+		{
+			event->ev_fd		= 	fd;
+			event->ev_flags		=	flags;
+			event->ev_arg		=	arg;
+			event->ev_handler	=	handler;
+		}		
+	}
+}
+
+/* Add event flags */
+void event_add(EVENT *event, short flags)
+{
+	if(event)
+	{
+        event->old_ev_flags = event->ev_flags;
+		event->ev_flags |= flags;
+		if(event->ev_base && event->ev_base->update)
+		{
+			event->ev_base->update(event->ev_base, event);
+			DEBUG_LOGGER(event->ev_base->logger, 
+                    "Added event[%08x] flags[%d] on fd[%d]",
+				event, event->ev_flags, event->ev_fd);
+		}
+	}
+}
+
+/* Delete event flags */
+void event_del(EVENT *event, short flags)
+{
+	if(event)
+	{
+		if(event->ev_flags & flags)
+		{
+            event->old_ev_flags = event->ev_flags;
+			event->ev_flags ^= flags;
+			if(event->ev_base && event->ev_base->update)
+			{
+				event->ev_base->update(event->ev_base, event);
+				DEBUG_LOGGER(event->ev_base->logger, "Updated event[%08x] flags[%d] on fd[%d]",
+					event, event->ev_flags, event->ev_fd);
+			}
+
+		}
+	}	
+}
+
+/* Destroy event */
+void event_destroy(EVENT *event)
+{
+    if(event)
+    {
+        event->ev_flags = 0;
+        if(event->ev_base && event->ev_base->del)
+        {
+            event->ev_base->del(event->ev_base, event);
+            DEBUG_LOGGER(event->ev_base->logger, "Destroy event[%08x] on fd[%d]",
+                    event, event->ev_fd);
+            event->ev_base = NULL;
+        }
+    }
+}
+
+/* Active event */
+void event_active(EVENT *event, short ev_flags)
+{
+	int n = 16;
+	char buf[16];
+	short e_flags = ev_flags;
+	if(event && event->ev_handler)
+	{
+		DEBUG_LOGGER(event->ev_base->logger, 
+			"Activing event[%08x] flags[%d] on fd[%d]", event, e_flags, event->ev_fd);
+		event->ev_handler(event->ev_fd, e_flags, event->ev_arg);	
+		if(!(event->ev_flags & E_PERSIST))
+		{
+			event->destroy(event);
+		}
+	}
+}
+
+/* Clean event */
+void event_clean(EVENT **event)
+{
+	if(*event)
+	{
+		free((*event));
+		(*event) = NULL;	
+	}
+}
