7818f57bd98816318f333e8f8f37419311a82c06
sounos <sounos@7d4f2944-f531-0410-9e72-0badb3dd7d62>
2010-01-19 05:54:09 +0000

    updated benchmark.c

--- devel/libsbase/src/benchmark.c
+++ devel/libsbase/src/benchmark.c
@@ -10,8 +10,64 @@ static SBASE *sbase = NULL;
 static SERVICE *service = NULL;
 static int concurrent = 1;
 static int nconns = 1024;
+static int nrequests = 0;
 static int nfailed = 0;
+static int nerrors = 0;
 static int is_keepalive = 1;
+static char *server_ip = NULL;
+static int server_port = 0;
+static int server_is_ssl = 0;
+static char request[HTTP_BUF_SIZE];
+static int request_len = 0; 
+
+CONN *http_newconn(char *ip, int port, int is_ssl)
+{
+    CONN *conn = NULL;
+
+    if(nrequests < nconns && ip && port > 0)
+    {
+        if(is_ssl) service->session.is_use_SSL = 1;
+        if((conn = service->newconn(service, -1, -1, ip, port, NULL)))
+        {
+            id = ++nrequests;
+            conn->start_cstate(conn);
+            service->newtransaction(service, conn, id);
+        }
+    }
+    return conn;
+}
+
+/* http request */
+int http_request(CONN *conn)
+{
+    if(conn && request_len > 0)
+    {
+        return conn->push_chunk(conn, request, request_len);
+    }
+    return -1;
+}
+
+/* http over */
+int http_over(CONN *conn, int respcode)
+{
+    if(conn)
+    {
+        if(respcode < 200 || respcode >= 300)
+        {
+            nerrors++;
+            conn->over_cstate(conn);
+            conn->over(conn);
+            if((conn = http_newconn(server_ip, server_port, server_is_ssl)))
+                return 0;
+        }
+        else
+        {
+            ncompleted++;
+            http_request(conn);
+        }
+    }
+    return -1;
+}
 
 int benchmark_packet_reader(CONN *conn, CB_DATA *buffer)
 {
@@ -20,9 +76,41 @@ int benchmark_packet_reader(CONN *conn, CB_DATA *buffer)
 
 int benchmark_packet_handler(CONN *conn, CB_DATA *packet)
 {
-	if(conn && conn->push_chunk)
+    char *p = NULL, *end = NULL, *s = NULL;
+    int respcode = -1;
+
+	if(conn)
     {
-		return conn->push_chunk((CONN *)conn, ((CB_DATA *)packet)->data, packet->ndata);
+        p = packet->data;
+        end = packet->end;
+        //check response code 
+        if((s = strstr(p, "HTTP/")))
+        {
+            s += 5;
+            while(*s != 0x20 && s < end)++s;
+            while(*s == 0x20)++s;
+            if(*s >= '0' && *s <= '9') respcode = atoi(s);
+        }
+        conn->sid = respcode;
+        if(respcode < 200 || respcode > 300)
+        {
+            return http_over(conn, respcode);
+        }
+        //check Content-Length
+        if((s = strstr(p, "Content-Length")) || (s = strstr(p, "content-length")))
+        {
+            s += 14;
+            while(s < end)
+            {
+                if(*s >= '0' && *s <= '9')break;
+                else++s;
+            }
+            if(*s >= '0' && *s <= '9') 
+                conn->recv_chunk(conn, atoll(s));
+            else 
+                return http_request(conn);
+            
+        }
     }
     return -1;
 }
@@ -31,7 +119,7 @@ int benchmark_data_handler(CONN *conn, CB_DATA *packet, CB_DATA *cache, CB_DATA 
 {
     if(conn)
     {
-
+        return http_over(conn, conn->sid);
     }
 }
 
@@ -39,7 +127,7 @@ int benchmark_error_handler(CONN *conn, CB_DATA *packet, CB_DATA *cache, CB_DATA
 {
     if(conn)
     {
-
+        return http_over(conn, conn->sid);
     }
 }
 
@@ -47,16 +135,15 @@ int benchmark_timeout_handler(CONN *conn, CB_DATA *packet, CB_DATA *cache, CB_DA
 {
     if(conn)
     {
-
+        return http_over(conn, conn->sid);
     }
 }
 
 int benchmark_oob_handler(CONN *conn, CB_DATA *oob)
 {
-    if(conn && conn->push_chunk)
+    if(conn)
     {
-        conn->push_chunk((CONN *)conn, ((CB_DATA *)oob)->data, oob->ndata);
-        return oob->ndata;
+        return 0;
     }
     return -1;
 }
