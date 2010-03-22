a7e0e5e679848a3bfc0d3025052e7c25f95f02b3
sounos <sounos@7d4f2944-f531-0410-9e72-0badb3dd7d62>
2010-03-22 15:20:50 +0000

    updated procthread_run();

--- trunk/devel/libsbase/src/procthread.c
+++ trunk/devel/libsbase/src/procthread.c
@@ -31,7 +31,7 @@ void procthread_run(void *arg)
             {
                 if(QMTOTAL(pth->message_queue) > 0)
                     qmessage_handler(pth->message_queue, pth->logger);
-                else usleep(10);
+                else usleep(100);
             }while(pth->running_status);
         }
         if(QMTOTAL(pth->message_queue) > 0)
