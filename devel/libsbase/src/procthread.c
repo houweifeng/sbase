2391ca08738148e962dd7fb574431752afff58d1
sounos <sounos@7d4f2944-f531-0410-9e72-0badb3dd7d62>
2010-03-23 05:30:36 +0000

    rebuild configure

--- devel/libsbase/src/procthread.c
+++ devel/libsbase/src/procthread.c
@@ -23,6 +23,7 @@ void procthread_run(void *arg)
                 pth->evbase->loop(pth->evbase, 0, &tv);
                 if(QMTOTAL(pth->message_queue) > 0)
                     qmessage_handler(pth->message_queue, pth->logger);
+                else usleep(10);
             }while(pth->running_status);
         }
         else
@@ -31,7 +32,7 @@ void procthread_run(void *arg)
             {
                 if(QMTOTAL(pth->message_queue) > 0)
                     qmessage_handler(pth->message_queue, pth->logger);
-                else usleep(pth->usec_sleep);
+                else usleep(10);
             }while(pth->running_status);
         }
         if(QMTOTAL(pth->message_queue) > 0)
