9768073a4f3ff5cfdd97714de20ae84e40916bbb
sounos <sounos@7d4f2944-f531-0410-9e72-0badb3dd7d62>
2010-03-23 04:13:23 +0000

    updated do{}while(0);

--- devel/libsbase/src/procthread.c
+++ devel/libsbase/src/procthread.c
@@ -23,7 +23,6 @@ void procthread_run(void *arg)
                 pth->evbase->loop(pth->evbase, 0, &tv);
                 if(QMTOTAL(pth->message_queue) > 0)
                     qmessage_handler(pth->message_queue, pth->logger);
-                else usleep(10);
             }while(pth->running_status);
         }
         else
@@ -35,8 +34,8 @@ void procthread_run(void *arg)
                 else usleep(pth->usec_sleep);
             }while(pth->running_status);
         }
-        //if(QMTOTAL(pth->message_queue) > 0)
-        //    qmessage_handler(pth->message_queue, pth->logger);
+        if(QMTOTAL(pth->message_queue) > 0)
+            qmessage_handler(pth->message_queue, pth->logger);
     }
 #ifdef HAVE_PTHREAD
     pthread_exit(NULL);
