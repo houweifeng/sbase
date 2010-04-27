f55d2b71ddad9a1756818e1e8d068c8bd32cf626
sounos <sounos@7d4f2944-f531-0410-9e72-0badb3dd7d62>
2010-04-27 06:30:53 +0000

    updated evbase->loop();

--- devel/libsbase/src/procthread.c
+++ devel/libsbase/src/procthread.c
@@ -28,7 +28,7 @@ void procthread_run(void *arg)
         {
             do
             {
-                pth->evbase->loop(pth->evbase, 0, &tv);
+                if(pth->evbase->loop(pth->evbase, 0, &tv) <= 0) usleep(1);
                 if(pth->message_queue && QMTOTAL(pth->message_queue) > 0)
                     qmessage_handler(pth->message_queue, pth->logger);
             }while(pth->running_status);
