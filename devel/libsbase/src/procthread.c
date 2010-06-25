cd12f5189b7f5e95cbfaed4b8c24d9a8813798e3
sounos <sounos@7d4f2944-f531-0410-9e72-0badb3dd7d62>
2010-06-25 03:39:04 +0000

    updated procthread.c

--- devel/libsbase/src/procthread.c
+++ devel/libsbase/src/procthread.c
@@ -54,8 +54,8 @@ void procthread_run(void *arg)
                     qmessage_handler(pth->message_queue, pth->logger);
                 else
                 {
-                    usleep(pth->usec_sleep);
-                    //ret = MUTEX_WAIT(pth->mutex);
+                    //usleep(pth->usec_sleep);
+                    ret = MUTEX_WAIT(pth->mutex);
                     //fprintf(stdout, "%s::%d mutex:%p ret:%d, %s\n", __FILE__, __LINE__, pth->mutex, ret,  strerror(errno));
                 }
             }while(pth->running_status);
