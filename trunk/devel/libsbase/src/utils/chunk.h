10c5fce2a056fbcac1c5c386e27add2a1cddc98c
sounos <sounos@7d4f2944-f531-0410-9e72-0badb3dd7d62>
2010-09-06 01:31:41 +0000

    updated chunk.h

--- trunk/devel/libsbase/src/utils/chunk.h
+++ trunk/devel/libsbase/src/utils/chunk.h
@@ -11,7 +11,7 @@
 #define CHUNK_FILE  0x04
 #define CHUNK_ALL  (CHUNK_MEM | CHUNK_FILE)
 #define CHUNK_FILE_NAME_MAX 1024 
-#define CHUNK_BLOCK_MAX         262144
+#define CHUNK_BLOCK_MAX         1048576
 #ifndef MMAP_CHUNK_SIZE
 #define MMAP_CHUNK_SIZE         1048576
 //#define MMAP_CHUNK_SIZE       2097152
