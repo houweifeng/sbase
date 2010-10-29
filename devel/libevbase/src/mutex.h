c3a914a74a18e4c73e4b826a2cf384982bf508f1
sounos <sounos@7d4f2944-f531-0410-9e72-0badb3dd7d62>
2010-10-29 08:01:56 +0000

    added xmm.c

--- devel/libevbase/src/mutex.h
+++ devel/libevbase/src/mutex.h
@@ -3,12 +3,10 @@
 #ifdef HAVE_PTHREAD
 #include <pthread.h>
 #endif
-#ifdef HAVE_MMAP
-#include <sys/mman.h>
-#endif
 #ifdef __cplusplus
 extern "C" {
 #endif
+#include "xmm.h"
 //#ifdef USE_PTHREAD_MUTEX
 #ifdef HAVE_PTHREAD
 typedef struct _MUTEX
@@ -17,35 +15,19 @@ typedef struct _MUTEX
     pthread_cond_t  cond;
 }MUTEX;
 #define MT(ptr) ((MUTEX *)ptr)
-#ifdef HAVE_MMAP
 #define MUTEX_INIT(ptr)                                                     \
 do                                                                          \
 {                                                                           \
-    if((ptr = mmap(NULL, sizeof(MUTEX), PROT_READ|PROT_WRITE,               \
-                    MAP_ANON|MAP_PRIVATE, -1, 0)) && ptr != (void *)-1)     \
-    {                                                                       \
-        memset(ptr, 0, sizeof(MUTEX));                                      \
-        pthread_mutex_init(&(MT(ptr)->mutex), NULL);                        \
-        pthread_cond_init(&(MT(ptr)->cond), NULL);                          \
-    }                                                                       \
-    else ptr = NULL;                                                        \
-}while(0)
-#else
-#define MUTEX_INIT(ptr)                                                     \
-do                                                                          \
-{                                                                           \
-    if((ptr = calloc(1, sizeof(MUTEX))))                                    \
+    if((ptr = xmm_new(sizeof(MUTEX))))                                      \
     {                                                                       \
         pthread_mutex_init(&(MT(ptr)->mutex), NULL);                        \
         pthread_cond_init(&(MT(ptr)->cond), NULL);                          \
     }                                                                       \
 }while(0)
-#endif
 #define MUTEX_LOCK(ptr) ((ptr) ? pthread_mutex_lock(&(MT(ptr)->mutex)): -1)
 #define MUTEX_UNLOCK(ptr) ((ptr) ? pthread_mutex_unlock(&(MT(ptr)->mutex)): -1)
 #define MUTEX_WAIT(ptr) ((ptr && pthread_mutex_lock(&(MT(ptr)->mutex)) == 0 && pthread_cond_wait(&(MT(ptr)->cond), &(MT(ptr)->mutex)) == 0 && pthread_mutex_unlock(&(MT(ptr)->mutex)) == 0)? 0 : -1)
 #define MUTEX_SIGNAL(ptr) ((ptr)?pthread_cond_signal(&(MT(ptr)->cond)): -1)
-#ifdef HAVE_MMAP
 #define MUTEX_DESTROY(ptr)                                                  \
 do                                                                          \
 {															                \
@@ -53,23 +35,10 @@ do                                                                          \
 	{														                \
 		pthread_mutex_destroy(&(MT(ptr)->mutex)); 						    \
 		pthread_cond_destroy(&(MT(ptr)->cond)); 							\
-		munmap(ptr, sizeof(MUTEX));											\
+		xmm_free(ptr, sizeof(MUTEX));										\
 		ptr = NULL;												            \
 	}														                \
 }while(0)
-#else
-#define MUTEX_DESTROY(ptr)                                                  \
-do                                                                          \
-{															                \
-	if(ptr)													                \
-	{														                \
-		pthread_mutex_destroy(&(MT(ptr)->mutex)); 						    \
-		pthread_cond_destroy(&(MT(ptr)->cond)); 							\
-		free(ptr);											                \
-		ptr = NULL;												            \
-	}														                \
-}while(0)
-#endif
 #else
 #ifdef HAVE_SEMAPHORE
 #include <semaphore.h>
@@ -81,7 +50,7 @@ typedef struct _MUTEX
 #define MUTEX_INIT(ptr)                                                     \
 do                                                                          \
 {                                                                           \
-    if((ptr = calloc(1, sizeof(MUTEX))))                                    \
+    if((ptr = xmm_new(sizeof(MUTEX))))                                      \
     {                                                                       \
         sem_init(&(MT(ptr)->sem), 0, 1);                                    \
     }                                                                       \
@@ -93,7 +62,7 @@ do                                                                          \
 #define MUTEX_DESTROY(ptr)                                                  \
 do                                                                          \
 {                                                                           \
-    if(ptr){sem_destroy(&(MT(ptr)->sem));free(ptr);}                        \
+    if(ptr){sem_destroy(&(MT(ptr)->sem));xmm_free(ptr, sizeof(MUTEX));}     \
 }while(0)
 #else
 #define MUTEX_INIT(ptr)
