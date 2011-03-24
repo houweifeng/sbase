4cbc4b7be205d418df0eff620c372653d2c791c0
sounos <sounos@7d4f2944-f531-0410-9e72-0badb3dd7d62>
2011-03-24 06:50:17 +0000

    updated sbase.h

--- trunk/devel/libsbase/src/sbase.h
+++ trunk/devel/libsbase/src/sbase.h
@@ -22,7 +22,7 @@ extern "C" {
 #define SB_SERVICE_MAX          256
 #define SB_THREADS_MAX          256
 #define SB_INIT_CONNS           256
-#define SB_QCONN_MAX            2048
+#define SB_QCONN_MAX            256
 #define SB_CHUNKS_MAX           256
 #define SB_BUF_SIZE             65536
 #define SB_USEC_SLEEP           1000
