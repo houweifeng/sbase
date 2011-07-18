2a0a2af654b27e0caffc8010d6a06d3d13a5c367
sounos <sounos@7d4f2944-f531-0410-9e72-0badb3dd7d62>
2011-07-18 08:57:12 +0000

    updated evbase.c

--- trunk/devel/libevbase/src/evepoll.c
+++ trunk/devel/libevbase/src/evepoll.c
@@ -161,7 +161,7 @@ int evepoll_loop(EVBASE *evbase, int loop_flags, struct timeval *tv)
         }
         //memset(evbase->evs, 0, sizeof(struct epoll_event) * evbase->allowed);
         //n = epoll_wait(evbase->efd, evbase->evs, evbase->maxfd+1, timeout);
-        n = epoll_wait(evbase->efd, (struct epoll_event *)evbase->evs, evbase->maxfd+1, timeout);
+        n = epoll_wait(evbase->efd, (struct epoll_event *)evbase->evs, evbase->nevents, timeout);
         if(n <= 0)
         {
             if(n < 0){fprintf(stderr, "epoll_wait(%d, %p, %d, %d) failed, %s\n", evbase->efd, evbase->evs, evbase->nevents, timeout, strerror(errno));}
