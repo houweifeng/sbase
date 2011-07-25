d79ea5533a8d1278d35be4704a16b6f81f8efae7
sounos <sounos@7d4f2944-f531-0410-9e72-0badb3dd7d62>
2011-07-25 13:38:03 +0000

    updated xmm.c

--- devel/libsbase/src/utils/xmm.c
+++ devel/libsbase/src/utils/xmm.c
@@ -12,11 +12,9 @@ void *xmm_mnew(size_t size)
     void *m = NULL;
     if(size > 0)
     {
-        /*
         if(size < M_PAGE_SIZE)
             m = calloc(1, size);
         else
-        */
         {
             m = mmap(NULL, MMSIZE(size), PROT_READ|PROT_WRITE, MAP_ANON|MAP_PRIVATE, -1, 0);
             if(m == NULL || m == (void *)-1) m = NULL;
@@ -31,11 +29,9 @@ void *xmm_new(size_t size)
     void *m = NULL;
     if(size > 0)
     {
-        /*
         if(size < M_PAGE_SIZE)
             m = calloc(1, size);
         else
-        */
         {
             m = mmap(NULL, MMSIZE(size), PROT_READ|PROT_WRITE, MAP_ANON|MAP_PRIVATE, -1, 0);
             if(m == NULL || m == (void *)-1) m = NULL;
@@ -50,13 +46,11 @@ void *xmm_mresize(void *old, size_t old_size, size_t new_size)
     void *m = NULL;
     if(new_size > 0 && new_size > old_size)
     {
-        /*
         if(new_size < M_PAGE_SIZE)
         {
             m = calloc(1, new_size);
         }
         else
-        */
         {
             m = mmap(NULL, MMSIZE(new_size), PROT_READ|PROT_WRITE, MAP_ANON|MAP_PRIVATE, -1, 0);
             if(m == (void *)-1) m = NULL;
@@ -66,8 +60,8 @@ void *xmm_mresize(void *old, size_t old_size, size_t new_size)
             if(old_size > 0)
             {
                 if(m) memcpy(m, old, old_size);
-                //if(old_size < M_PAGE_SIZE) free(old);
-                //else 
+                if(old_size < M_PAGE_SIZE) free(old);
+                else 
                     munmap(old, MMSIZE(old_size));
             }
         }
@@ -80,13 +74,11 @@ void *xmm_resize(void *old, size_t old_size, size_t new_size)
     void *m = NULL;
     if(new_size > 0 && new_size > old_size)
     {
-        /*
         if(new_size < M_PAGE_SIZE)
         {
             m = calloc(1, new_size);
         }
         else
-        */
         {
             m = mmap(NULL, MMSIZE(new_size), PROT_READ|PROT_WRITE, MAP_ANON|MAP_PRIVATE, -1, 0);
             if(m == (void *)-1) m = NULL;
@@ -96,8 +88,8 @@ void *xmm_resize(void *old, size_t old_size, size_t new_size)
             if(old_size > 0)
             {
                 if(m) memcpy(m, old, old_size);
-                //if(old_size < M_PAGE_SIZE) free(old);
-                //else
+                if(old_size < M_PAGE_SIZE) free(old);
+                else
                     munmap(old, MMSIZE(old_size));
             }
         }
@@ -124,8 +116,8 @@ void xmm_free(void *m, size_t size)
 {
     if(m && size > 0)
     {
-        //if(size < M_PAGE_SIZE) free(m);
-        //else 
+        if(size < M_PAGE_SIZE) free(m);
+        else 
             munmap(m, MMSIZE(size));
     }
     return ;
