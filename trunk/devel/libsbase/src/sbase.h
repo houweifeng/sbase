ef144d05cf2908cb985ff99390a97a31e7326e0a
sounos <sounos@7d4f2944-f531-0410-9e72-0badb3dd7d62>
2011-07-25 02:23:12 +0000

    updated service.c

--- trunk/devel/libsbase/src/sbase.h
+++ trunk/devel/libsbase/src/sbase.h
@@ -265,7 +265,7 @@ typedef struct _SERVICE
     /* working mode */
     struct _PROCTHREAD *daemon;
     struct _PROCTHREAD *acceptor;
-    struct _PROCTHREAD *recover;
+    struct _PROCTHREAD *tracker;
     struct _PROCTHREAD *iodaemons[SB_THREADS_MAX];
     struct _PROCTHREAD *procthreads[SB_THREADS_MAX];
     struct _PROCTHREAD *daemons[SB_THREADS_MAX];
