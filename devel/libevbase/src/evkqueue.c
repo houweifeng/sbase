2441bc544a8a32378ee6add74d4a4aaca6b03a6e
sounos <sounos@7d4f2944-f531-0410-9e72-0badb3dd7d62>
2011-08-03 11:34:03 +0000

    updated evkqueue.c

--- devel/libevbase/src/evkqueue.c
+++ devel/libevbase/src/evkqueue.c
@@ -206,7 +206,7 @@ int evkqueue_loop(EVBASE *evbase, int loop_flags, struct timeval *tv)
                 if(kqev->filter == EVFILT_READ)	ev_flags |= E_READ;
                 else if(kqev->filter == EVFILT_WRITE) ev_flags |= E_WRITE;
                 if(ev_flags == 0) continue;
-                if(ev == evbase->evlist[kqev->ident] && (ev_flags &=  ev->ev_flags)) 
+                if(ev == evbase->evlist[kqev->ident]) 
                 {
                     event_active(ev, ev_flags);
                 }
