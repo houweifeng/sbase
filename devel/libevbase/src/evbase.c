f441ad84eb8134c574910e6603669da4b9c64202
sounos <sounos@7d4f2944-f531-0410-9e72-0badb3dd7d62>
2011-08-03 10:24:52 +0000

    fixed evbase.c

--- devel/libevbase/src/evbase.c
+++ devel/libevbase/src/evbase.c
@@ -229,7 +229,7 @@ void event_del(EVENT *event, int flags)
 		{
             //if(event->ev_flags & E_LOCK) fprintf(stdout, "%s::%d ev_fd:%d del_event:%d ev_flags:%d old_ev_flags:%d\n", __FILE__, __LINE__, event->ev_fd, flags, event->ev_flags, event->old_ev_flags);
             event->old_ev_flags = event->ev_flags;
-			event->ev_flags ^= flags;
+			event->ev_flags &= ~flags;
             if(event->ev_base && event->ev_base->update)
 			{
                 event->ev_base->update(event->ev_base, event);
