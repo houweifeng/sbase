d58d9d7a752675aed8aab76a68478eb233cfd4bb
sounos <sounos@7d4f2944-f531-0410-9e72-0badb3dd7d62>
2011-08-10 08:11:35 +0000

    updated evepoll.c

--- devel/libevbase/src/evkqueue.c
+++ devel/libevbase/src/evkqueue.c
@@ -192,8 +192,8 @@ int evkqueue_loop(EVBASE *evbase, int loop_flags, struct timeval *tv)
     if(evbase)
     {
         if(tv) {TIMEVAL_TO_TIMESPEC(tv, &ts); pts = &ts;}
-        //n = kevent(evbase->efd, NULL, 0, (struct kevent *)evbase->evs, evbase->allowed, pts);	
-        n = kevent(evbase->efd, NULL, 0, (struct kevent *)evbase->evs, evbase->maxfd+1, pts);	
+        n = kevent(evbase->efd, NULL, 0, (struct kevent *)evbase->evs, evbase->allowed, pts);	
+        //n = kevent(evbase->efd, NULL, 0, (struct kevent *)evbase->evs, evbase->maxfd+1, pts);	
         if(n <= 0 )return n;
         for(i = 0; i < n; i++)
         {
