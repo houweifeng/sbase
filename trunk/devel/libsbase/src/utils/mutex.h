655704a3aec43e89d112dd2a09989866d8659a3a
sounos <sounos@7d4f2944-f531-0410-9e72-0badb3dd7d62>
2011-08-26 12:37:19 +0000

    updated mutex.h conn.c

--- trunk/devel/libsbase/src/utils/mutex.h
+++ trunk/devel/libsbase/src/utils/mutex.h
@@ -73,8 +73,9 @@ do{if(m)                                                                        
 do{if(m)                                                                            \
 {                                                                                   \
     pthread_mutex_lock(__MM__(m));                                                  \
-    if(__M__(m)->nowait == 0) __M__(m)->nowait = 1;                                 \
-    pthread_cond_signal(__MC__(m));                                                 \
+    if(__M__(m)->nowait == 0)                                                       \
+        pthread_cond_signal(__MC__(m));                                             \
+    __M__(m)->nowait = 1;                                                           \
     pthread_mutex_unlock(__MM__(m));                                                \
 }}while(0)
 #define MUTEX_DESTROY(m)                                                            \
