5c7d0ebd47f4edd898a8e281e9a8ea7423ea2cbc
sounos <sounos@7d4f2944-f531-0410-9e72-0badb3dd7d62>
2011-08-29 02:55:56 +0000

    updated mutex.h

--- devel/libevbase/src/mutex.h
+++ devel/libevbase/src/mutex.h
@@ -35,7 +35,8 @@ typedef struct _MUTEX
 #define MUTEX_UNLOCK(m) ((m)?sem_post(__MM__(m)):-1)
 #define MUTEX_WAIT(m) ((m)?(sem_wait(__MC__(m))):-1)
 #define MUTEX_TIMEDWAIT(m, ts) ((m)?sem_timedwait(__MC__(m), &ts):-1)
-#define MUTEX_SIGNAL(m) ((m && sem_getvalue(__MC__(m), __MV__(m)) == 0 && ((MUTEX *)m)->val < 1)?sem_post(__MC__(m)):-1)
+#define MUTEX_SIGNAL(m) ((m)?sem_post(__MC__(m)):-1)
+//#define MUTEX_SIGNAL(m) ((m && sem_getvalue(__MC__(m), __MV__(m)) == 0 && ((MUTEX *)m)->val < 1)?sem_post(__MC__(m)):-1)
 #define MUTEX_DESTROY(m) do{if(m){sem_destroy(__MM__(m));sem_destroy(__MC__(m));free(m);}}while(0)
 #else
 #define __M__(m) ((MUTEX *)m)
@@ -72,11 +73,8 @@ do{if(m)                                                                        
 do{if(m)                                                                            \
 {                                                                                   \
     pthread_mutex_lock(__MM__(m));                                                  \
-    if(__M__(m)->nowait == 0)                                                       \
-    {                                                                               \
-        __M__(m)->nowait = 1;                                                       \
-        pthread_cond_signal(__MC__(m));                                             \
-    }                                                                               \
+    __M__(m)->nowait = 1;                                                           \
+    pthread_cond_signal(__MC__(m));                                                 \
     pthread_mutex_unlock(__MM__(m));                                                \
 }}while(0)
 #define MUTEX_DESTROY(m)                                                            \
