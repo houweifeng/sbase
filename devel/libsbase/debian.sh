0a3106cd1125af67590667f69d9be8a016777262
sounos <sounos@7d4f2944-f531-0410-9e72-0badb3dd7d62>
2012-08-29 08:28:40 +0000

    updated debian.sh

--- devel/libsbase/debian.sh
+++ devel/libsbase/debian.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pkg=`grep -E "^PACKAGE_NAME=" configure |sed -e "s/PACKAGE_NAME='\(.*\)'/\1/"`
 ver=`grep -E "^PACKAGE_VERSION=" configure |sed -e "s/PACKAGE_VERSION='\(.*\)'/\1/"`
-dh_make -l -n -c bsd -e SounOS@gmail.com 
+dh_make -s -n -c bsd -e SounOS@gmail.com 
