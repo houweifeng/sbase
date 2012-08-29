3162028399016e7b93ebc0c43d05bb98666ec6ad
sounos <sounos@7d4f2944-f531-0410-9e72-0badb3dd7d62>
2012-08-29 09:50:22 +0000

    updated debian.sh

--- devel/libevbase/debian.sh
+++ devel/libevbase/debian.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 pkg=`grep -E "^PACKAGE_NAME=" configure |sed -e "s/PACKAGE_NAME='\(.*\)'/\1/"`
 ver=`grep -E "^PACKAGE_VERSION=" configure |sed -e "s/PACKAGE_VERSION='\(.*\)'/\1/"`
-dh_make -s -n -c bsd -e SounOS@gmail.com 
+dh_make -s -n -c bsd -e SounOS@gmail.com -p ${pkg}_${ver}
+cp control debian/control
+sed -i "s/^Package:.*/Package: $pkg/" debian/control
+sed -i "s/^Source:.*/Source: $pkg/" debian/control
+sed -i "s/^License:.*/License: $pkg/" debian/control
+sed -i "s/^Standards-Version:.*/Standards-Version: $ver/" debian/control
+dpkg-buildpackage -rfakeroot
